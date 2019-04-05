f62393cfaae6bc11e4aba2adeb69c0cadef3ec64
xxary <43515113+xxary@users.noreply.github.com>
2019-04-06 00:45:31 +0900

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,50 @@
+#include<vector>
+#include<string>
+#include<iostream>
+#include<cassert>
+
+template<typename T>
+class Stack{
+    private:
+    std::vector<T> elems;
+    
+    public:
+    void push(T const& elem);
+    void pop();
+    T const& top() const;
+    bool empty() const{
+        return elems.empty();
+    }
+};
+template<typename T>
+void Stack<T>::push(T const& elem){
+    elems.push_back(elem);
+}
+template<typename T>
+void Stack<T>::pop(){
+    assert(!elems.empty());
+    elems.pop_back();
+}
+template<typename T>
+T const& Stack<T>::top() const{
+    assert(!elems.empty());
+    return elems.back();
+}
+
+int main(){
+    Stack<std::string> kanashimi;
+    kanashimi.push("あ");
+    kanashimi.push("い");
+    kanashimi.push("う");
+    kanashimi.push("え");
+    std::cout<<kanashimi.top()<<'\n';
+    kanashimi.pop();
+    std::cout<<kanashimi.top()<<'\n';
+    kanashimi.pop();
+    std::cout<<kanashimi.top()<<'\n';
+    kanashimi.pop();
+    std::cout<<kanashimi.top()<<'\n';
+    kanashimi.pop();
+}
+    
+    
